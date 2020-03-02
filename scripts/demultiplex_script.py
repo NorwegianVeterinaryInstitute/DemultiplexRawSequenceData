9ead8525080781295494e218400b14717a6f1c44
arvindsundaram <arvindymsundaram@gmail.com>
2020-03-02 21:44:56 +0100

    Update demultiplex_script.py

--- scripts/demultiplex_script.py
+++ scripts/demultiplex_script.py
@@ -1,55 +1,60 @@
 import os, sys, subprocess
 
 def execute(command):
-    print ('	' + command)
-    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    command2 = 'source activate miseq && ' + command
+    print ('    ' + command2)
+    p = subprocess.Popen(command2, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p_status = p.wait()
 
-def checkComplete(RunId):
-    if not os.path.exists('/mnt/data/scratch/' + RunId + '/RTAComplete.txt'):
-        print ('Run is not complete')
-        return ('False')
-    else:
+def checkComplete(RunLocation):
+    if os.path.exists(RunLocation + '/RTAComplete.txt'):
         return ('True')
-    
-def createDirectory(RunId):
-    execute('mkdir /mnt/data/demultiplex/' + RunId)
-    execute('mkdir /mnt/data/demultiplex/' + RunId + '/QC')
-    execute('mkdir /mnt/data/demultiplex/' + RunId + '/demultiplex_log')
-    print ('1/5 Tasks: Directories created')
+    else:
+        return ('False')
 
-def demutliplex(RunId):
+def createDirectory(DemultiplexLocation):
+    if os.path.isdir(DemultiplexLocation):
+        return('False')
+    else:
+        execute('mkdir ' + DemultiplexLocation)
+        execute('mkdir ' + DemultiplexLocation + '/QC')
+        execute('mkdir ' + DemultiplexLocation + '/demultiplex_log')
+        print ('1/5 Tasks: Directories created')
+
+def demutliplex(RunLocation, DemultiplexLocation):
     print ('2/5 Tasks: Demultiplexing started')
-    execute('bcl2fastq --runfolder-dir /mnt/data/scratch/' + RunId + ' --output-dir /mnt/data/demultiplex/' + RunId + ' 2> /mnt/data/demultiplex/' + RunId + '/demultiplex_log/02_demultiplex.log')
+    execute('bcl2fastq --runfolder-dir ' + RunLocation + ' --output-dir ' + DemultiplexLocation + ' 2> ' + DemultiplexLocation + '/demultiplex_log/02_demultiplex.log')
     print ('2/5 Tasks: Demultiplexing complete')
 
-def moveFiles(RunId):
-    for fastq in os.listdir('/mnt/data/demultiplex/' + RunId):
-        if 'fastq.gz' in fastq:
-            execute('mv /mnt/data/demultiplex/' + RunId + '/' + fastq + ' /mnt/data/demultiplex/' + RunId + '/' + '_'.join(RunId.split('_')[0:2]) + '.' + fastq)
+def moveFiles(DemultiplexLocation):
+    for root, dirs, files in os.walk(DemultiplexLocation):
+        for name in files:
+            if 'fastq.gz' in name:
+                execute('mv ' + root + '/' + name + ' ' + root + '/' + '_'.join(RunId.split('_')[0:2]) + '.' + name)
     print ('3/5 Tasks: Moving files complete')
 
-def qc(RunId):
-    execute('fastqc -t 4 /mnt/data/demultiplex/' + RunId + '/*fastq.gz -o /mnt/data/demultiplex/' + RunId + '/QC' + ' > /mnt/data/demultiplex/' + RunId + '/demultiplex_log/04_fastqc.log')
+def qc(DemultiplexLocation):
+    execute('fastqc -t 4 ' + DemultiplexLocation + '/*fastq.gz ' + DemultiplexLocation + '/*/*.fastq.gz ' + DemultiplexLocation + '/*/*/*.fastq.gz' + ' -o ' + DemultiplexLocation + '/QC' + ' > ' + DemultiplexLocation + '/demultiplex_log/04_fastqc.log')
     print ('4/5 Tasks: FastQC complete')
-    execute('multiqc /mnt/data/demultiplex/' + RunId + '/QC' + ' -o /mnt/data/demultiplex/' + RunId + '/QC' + ' 2> /mnt/data/demultiplex/' + RunId + '/demultiplex_log/05_multiqc.log')
+    execute('multiqc ' + DemultiplexLocation + '/QC' + ' -o ' + DemultiplexLocation + '/QC' + ' 2> ' + DemultiplexLocation + '/demultiplex_log/05_multiqc.log')
     print ('5/5 Tasks: MultiQC complete')
 
-# def copyQC(RunId):
-#     execute('rsync /mnt/data/demultiplex/' + RunId + '/Stats ' + '/mnt/data/demultiplex/' + RunId + '/demultiplex_log ' + ' /mnt/data/scratch/' + RunId)
-#    execute('rsync /mnt/data/demultiplex/' + RunId + '/Reports ' + '/mnt/data/demultiplex/' + RunId + '/demultiplex_log ' + ' /mnt/data/scratch/' + RunId)
-
 def main(RunId):
-    if checkComplete(RunId) is 'True':
-        createDirectory(RunId)
-        demutliplex(RunId)
-        moveFiles(RunId)
-        qc(RunId)
-#    copyQC(RunId)
+    RunLocation = '/mnt/data/scratch/' + RunId
+    DemultiplexLocation = '/mnt/data/demultiplex/' + RunId + '_demultiplex'
+
+    if checkComplete(RunLocation) is 'False':
+        print (RunId + ' is not finished sequencing yet!!!')
+        sys.exit()
+    if createDirectory(DemultiplexLocation) is 'False':
+        print (DemultiplexLocation + ' exists. Delete or rename the demultiplex folder before re-running the script')
+        sys.exit()
+    demutliplex(RunLocation, DemultiplexLocation)
+    moveFiles(DemultiplexLocation)
+    qc(DemultiplexLocation)
+    print('\nAll done!')
 
 if __name__ == '__main__':
     RunId = sys.argv[1]
     main(RunId)
-    print('')
-    print('All done!')
