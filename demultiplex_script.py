e982de352b2f3b6204a169453d5fa039e85b5413
George Marselis <george.marselis@vetinst.no>
2022-10-10 01:25:03 +0200

    changed False to False, formatting

--- demultiplex_script.py
+++ demultiplex_script.py
@@ -1,6 +1,8 @@
 #!/bin/env /bin/python3
 
-import os, sys, subprocess
+import os
+import sys
+import subprocess
 
 # INPUTS:
 #   - Run name eg python /mnt/data/demultiplex/scripts/demultiplex_script_v2.py 200306_M06578_0015_000000000-CWLBG
@@ -21,6 +23,7 @@ import os, sys, subprocess
 #       - Relies only on output directory name and does not verify contents
 
 
+#
 def execute(command, demultiplex_out_file):
     # why is  export LANG=nb_NO.utf8 && export LC_ALL=nb_NO.utf8 there?
     command2 = 'source activate miseq && export LANG=nb_NO.utf8 && export LC_ALL=nb_NO.utf8 && ' + command
@@ -30,20 +33,29 @@ def execute(command, demultiplex_out_file):
     (output, err) = p.communicate()
     p_status = p.wait()
 
+
+#
 def checkComplete(RunFolder):
     if os.path.exists(RunFolder + '/RTAComplete.txt'):
-        return ('True')
+        return ( True )
     else:
-        return ('False')
+        return ( False )
 
+#
 def createDirectory(DemultiplexFolder, RunId_short):
-    if os.path.isdir(DemultiplexFolder):
-        return('False')
+    if os.path.isdir( DemultiplexFolder ):
+        return( False )
     else:
-        os.mkdir(DemultiplexFolder)
-        os.mkdir(DemultiplexFolder + '/' + RunId_short + '_QC')
-        os.mkdir(DemultiplexFolder + '/demultiplex_log')
+        QCSuffix    = '_QC'
+        DemuxLogDir = 'demultiplex_log'
+        QCDirectory = os.path.join( RunId_short + QCSuffix)
+        DemultiplexLogFile = os.path.join( ) 
+        os.mkdir( DemultiplexFolder )                              # root directory for run
+        os.mkdir( os.path.join( DemultiplexFolder, QCDirectory ) ) # QC directory   for run
+        os.mkdir( os.path.join( DemultiplexFolder, DemuxLogDir ) ) # log directory  for run
 
+
+#
 def demutliplex( RunFolder, DemultiplexFolder, demultiplex_out_file):
     demultiplex_out_file.write('2/5 Tasks: Demultiplexing started\n')
     execute('/bin/cp ' + RunFolder + '/SampleSheet.csv ' + DemultiplexFolder, demultiplex_out_file)
@@ -51,22 +63,26 @@ def demutliplex( RunFolder, DemultiplexFolder, demultiplex_out_file):
     execute('/usr/local/bin/bcl2fastq --no-lane-splitting --runfolder-dir ' + RunFolder + ' --output-dir ' + DemultiplexFolder + ' 2> ' + DemultiplexFolder + '/demultiplex_log/02_demultiplex.log', demultiplex_out_file)
     demultiplex_out_file.write('2/5 Tasks: Demultiplexing complete\n')
 
+#
 def getProjectName( DemultiplexFolder, demultiplex_out_file):
-    project_line_check = 'no'
-    project_index = ''
+
+    project_line_check = False
+    project_index  = ''
     analysis_index = ''
-    project_list = []
+    project_list   = []
 
     for line in open(DemultiplexFolder + '/SampleSheet.csv', 'r'):
         line = line.rstrip()
-        if project_line_check == 'yes':
+        if project_line_check == True:
             project_list.append(line.split(',')[project_index] + '.' + line.split(',')[analysis_index])
         if 'Sample_Project' in line:
-            project_index = line.split(',').index('Sample_Project')
-            analysis_index = line.split(',').index('Analysis')
-            project_line_check = 'yes'
-    return(set(project_list))
+            project_index      = line.split(',').index('Sample_Project')
+            analysis_index     = line.split(',').index('Analysis')
+            project_line_check = True
+    return( set( project_list ) )
 
+
+#
 def moveFiles(DemultiplexFolder, RunId_short, project_list, demultiplex_out_file):
     for root, dirs, files in os.walk(DemultiplexFolder):
         for name in files:
@@ -79,6 +95,8 @@ lex_out_file)
 
     demultiplex_out_file.write('3/5 Tasks: Moving files complete\n')
 
+
+#
 def qc(DemultiplexFolder, RunId_short, project_list, demultiplex_out_file):
     for project in project_list:
         project_folder = DemultiplexFolder + '/' + RunId_short + '.' + project
@@ -94,38 +112,46 @@ t_file)
     execute('/usr/local/bin/multiqc ' + DemultiplexFolder + '/' + RunId_short + '_QC' + ' -o ' + DemultiplexFolder + '/' + RunId_short + '_QC' + ' 2> ' + DemultiplexFolder + '/demultiplex_log/05_multiqc.log', demultiplex_out_file)
     demultiplex_out_file.write('5/5 Tasks: MultiQC complete\n')
 
+
+#
 def create_md5deep(Folder, demultiplex_out_file):
     md5deep_out = Folder + '/md5sum.txt'
     sed_command = '/bin/sed "s ' + Folder + '/  g" '
     execute('/bin/md5deep -r ' + Folder + ' | ' + sed_command + ' | grep -v md5sum | grep -v script > ' + md5deep_out, demultiplex_out_file)       
 
+#
 def script_completion_file(DemultiplexFolder, demultiplex_out_file):
     execute('/bin/touch ' + DemultiplexFolder + '/DemultiplexComplete.txt', demultiplex_out_file)
 
+
+#
 def prepare_delivery(folder, DemultiplexFolder , tar_file, md5_file, demultiplex_out_file):
     execute('/bin/tar -cvf ' + tar_file + ' -C ' + DemultiplexFolder + ' ' + folder , demultiplex_out_file)
     #sed_command = '/bin/sed "s /mnt/data/demultiplex/for_transfer/  g" '
     sed_command = '/bin/sed "s /data/for_transfer/  g" '
     execute('/bin/md5sum ' + tar_file + ' | ' + sed_command + ' > ' + md5_file, demultiplex_out_file)
 
+#
 def change_permission(folder_or_file, demultiplex_out_file):
     execute('/bin/chown -R sambauser01:sambagroup ' + folder_or_file, demultiplex_out_file)
     execute('/bin/chmod -R g+rwX sambagroup ' + folder_or_file, demultiplex_out_file)
 
+#
 def main(RunId):
 
-    DataFolder = '/data'
-    RunLocation = DataFolder + '/' + 'scratch'
+    DataFolder          = '/data'
+    RunLocation         = DataFolder + '/' + 'scratch'
     DemultiplexLocation = DataFolder + '/' + 'demultiplex'
 
-    RunId_short = '_'.join(RunId.split('_')[0:2])
-    RunFolder = RunLocation + '/' + RunId
+    RunId_short       = '_'.join(RunId.split('_')[0:2])
+    RunFolder         = RunLocation + '/' + RunId
     DemultiplexFolder = DemultiplexLocation + '/' + RunId + '_demultiplex'
 
-    if checkComplete(RunFolder) is 'False':
-        print (RunId + ' is not finished sequencing yet!!!')
+    if checkComplete(RunFolder) is False:
+        print (RunId + ' is not finished sequencing yet!')
         sys.exit()
-    if createDirectory(DemultiplexFolder, RunId_short) is 'False':
+
+    if createDirectory(DemultiplexFolder, RunId_short) is False:
         print (DemultiplexFolder + ' exists. Delete or rename the demultiplex folder before re-running the script')
         sys.exit()
     else:
