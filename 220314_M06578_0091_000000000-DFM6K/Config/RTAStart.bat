18ce409d5e58654e1742384260f44a7ab26f9d99
George Marselis <george.marselis@vetinst.no>
2023-01-10 13:21:32 +0100

    kot

--- 220314_M06578_0091_000000000-DFM6K/Config/RTAStart.bat
+++
@@ -1,3 +0,0 @@
-title RTA 1.18.54.4 - D:\Illumina\MiSeqTemp\220314_M06578_0091_000000000-DFM6K
-pushd "C:\Illumina\RTA"
-"C:\Illumina\RTA\RTA.exe"  "D:\Illumina\MiSeqTemp\220314_M06578_0091_000000000-DFM6K\Images " D:\Illumina\RTATemp Read=All ControlLane=0 Threads=2 CopyIntensityFilesToNetwork=0 instrumentType=miseq
