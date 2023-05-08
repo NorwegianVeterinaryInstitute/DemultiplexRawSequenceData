6b04e9f6f2737b8c9cbc01b61b03da2e3c0f20b9
George Marselis <george.marselis@vetinst.no>
2023-05-08 12:33:31 +0200

    added check that the tar files are packed correctly. we are done with #25

--- demultiplex_script.py
+++ demultiplex_script.py
@@ -1734,6 +1734,8 @@ def tarFileQualityCheck(  ):
     demux.n = demux.n + 1
     demuxLogger.info( termcolor.colored( f"==> {demux.n}/{demux.totalTasks} tasks: Tar files quaility check started ==", color="green", attrs=["bold"] ) )
 
+    forTransferRunIdDirTestName = os.path.join( demux.forTransferRunIdDir,demux.forTransferRunIdDirTestName )
+
 #---- Step 1: create a /data/for_transfer/RunID/test directory -------------------------------------------------------------------------------------------
 
     # ensure that demux.forTransferDir (/data/for_transfer) exists
@@ -1745,7 +1747,6 @@ def tarFileQualityCheck(  ):
         sys.exit( )    
 
     try: 
-        forTransferRunIdDirTestName = os.path.join( demux.forTransferRunIdDir,demux.forTransferRunIdDirTestName )
         os.mkdir( forTransferRunIdDirTestName )
     except Exception as err:
         text = f"{demux.forTransferRunIdDir} cannot be created: { str( err ) }\nExiting!"
@@ -1754,29 +1755,6 @@ def tarFileQualityCheck(  ):
         logging.shutdown( )
         sys.exit( )
 
-#---- Step 2: copy any tar file for relevant RunIDShort into the test directory ---------------------------------------------------------------------------
-
-    # we need a list of the .tar files inside demux.forTransferRunIdDir ( /data/for_transfer/220603_M06578_0105_000000000-KB7MY )
-    transferTarFiles = glob.glob( f"{demux.forTransferRunIdDir}/*.tar" )
-    # get a copy of the sha512 files, as well
-    transfterSha512Files = glob.glob( f"{demux.forTransferRunIdDir}/*.sha512")
-    #
-    # then copy them into forTransferRunIdDirTestName
-
-#---- Step 3: untar files under /data/for_transfer/RunID/test -------------------------------------------------------------------------------------------
-    for tarfile in demux.tarFilesToTransferList
-        try:
-            tarHandle = tarfile.open( name = tarfile, mode = 'r', fileobj = None, bufsize = 10240 )
-            tarHandle.extractall( forTransferRunIdDirTestName )
-            tarHandle.close( )
-        except Exception as err:
-            text = f"{forTransferRunIdDirTestName}/{tarfile} cannot be created: { str( err ) }\nExiting!"
-            demuxFailureLogger.critical( f"{ text }" )
-            demuxLogger.critical( f"{ text }" )
-            logging.shutdown( )
-            sys.exit( )
-
-
 # there is no point in making this complicated: tar files can be easily edited, they are just a simple container and any attacker can easily alter the file insitu,
 # recalculate the hash and replace the hash again in situ
 #
@@ -1784,16 +1762,27 @@ def tarFileQualityCheck(  ):
 # then delete the test_tar directory
 
 # for file in $TARFILES; do printf '\n==== tar file: $file============================='; tar --verbose --compare --file=$file | grep -v 'Mod time differs'; done
+#---- Step 2: untar all demux.tarFilesToTransferList in {demux.forTransferRunIdDir}/{demux.forTransferRunIdDirTestName} ------------------------------------------------------------
+    for tarFile in demux.tarFilesToTransferList:
+        try:
+            text = "Now extracting tarfile:"
+            demuxLogger.debug( f"{text:{demux.spacing3}}" + tarFile )
+            tarFileHandle = tarfile.open( name = tarFile, mode = "r:" )     # Open a tar file under  demux.forTransferRunIdDir as project + demux.tarSuffix . example: /data/for_transfer/220603_M06578_0105_000000000-KB7MY/220603_M06578.42015-NORM-VET.tar
+            tarFileHandle.extractall( path = forTransferRunIdDirTestName  )
+            tarFileHandle.close( )
+        except Exception as err:
+            text = f"{forTransferRunIdDirTestName}/{tarFile} cannot be created: { str( err ) }\nExiting!"
+            demuxFailureLogger.critical( f"{ text }" )
+            demuxLogger.critical( f"{ text }" )
+            logging.shutdown( )
+            sys.exit( )
 
-
-#---- Step 6: delete {demux.forTransferRunIdDir}/{demux.forTransferRunIdDirTestName} and contents ------------------------------------------------------------
+#---- Step 3: delete {demux.forTransferRunIdDir}/{demux.forTransferRunIdDirTestName} and contents ------------------------------------------------------------
     # clean up
-    cleanupPath = os.path.join( demux.forTransferRunIdDir, demux.forTransferRunIdDirTestName )
     text = "Cleanup up path:"
-    demuxLogger.info( f"{text:{demux.spacing2}}" + cleanupPath )
-    shutil.rmtree( cleanupPath )
+    demuxLogger.info( f"{text:{demux.spacing2}}" + forTransferRunIdDirTestName )
+    shutil.rmtree( forTransferRunIdDirTestName )
 
-#---- Step 7: return True/false depending on answer ------------------------------------------------------------------------------------------------------
 
     demuxLogger.info( termcolor.colored( f"==> {demux.n}/{demux.totalTasks} tasks: Tar files quaility check finished ==", color="red", attrs=["bold"] ) )
 
