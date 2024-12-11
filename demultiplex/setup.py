9425eb14815c38131a88ffa544b698cdcc91c9c5
George Marselis <george.marselis@vetinst.no>
2024-12-11 20:41:54 +0100

    information about inotify/watchdog

--- demultiplex/setup.py
+++ demultiplex/setup.py
@@ -13,9 +13,22 @@ setup(
     install_requires=[
 #Caution: This can cause compatibility issues if a future version of multiqc 
 # introduces breaking changes. Use version pinning (multiqc<2.0) if stability is a concern.
-        "multiqc"  # Specify minimum version if needed
+        "multiqc",  # Specify minimum version if needed
+        "watchdog", # use watchdog (which uses inotify) to watch for files being created
     ],
 )
+
+# inotify has performance issues with large directories, but "large" here means hundreds of
+# thousands of files or directories, or a watchlist exceeding the default system limit
+# (fs.inotify.max_user_watches, typically 8,192–128,000). Beyond this, performance degrades.
+
+# 201218_M06578_0041_000000000-JF7TM/ has 143,723 files and directories. That should work
+# with inotify but we might need to increase fs.inotify.max_user_watches:
+# 
+# sudo sysctl -w fs.inotify.max_user_watches=524288
+# do make this permament 
+# echo "fs.inotify.max_user_watches=524288" | sudo tee -a /etc/sysctl.conf
+
 
 # Use a systemd/ directory for clarity.
 # Use data_files to define the installation location.
