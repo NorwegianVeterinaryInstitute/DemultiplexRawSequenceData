4eb197294ba9f0615bc22956634430bcca92a158
George Marselis <george.marselis@vetinst.no>
2025-09-26 09:40:14 +0200

    finally reached page 350, taking script into modules

--- demultiplex_script.py
+++ demultiplex_script.py
@@ -42,6 +42,8 @@ from inspect import currentframe, getframeinfo
 
 
 from demux import demux # the demux object is where the whole initilization happens. read the top of demux/demux.py for more into
+from demux.util.buffering_smtp_handler import BufferingSMTPHandler
+
 
 """
 demultiplex_script.py:
@@ -180,42 +182,6 @@ LIMITATIONS
     - Relies only on output directory name and does not verify contents
 
 """
-
-
-########################################################################
-# BufferingSMTPHandlerft3
-########################################################################
-
-class BufferingSMTPHandler( logging.handlers.BufferingHandler ):
-    """
-    Instead of immediatelly sending email for notifications, buffer the ouput and send it at the end.
-    That way, you send one email instead of a multitude.
-    """
-    def __init__( self, mailhost, fromaddr, toaddrs, subject ):
-        logging.handlers.BufferingHandler.__init__( self, capacity = 9999999 )
-        self.mailhost = mailhost
-        self.mailport = None
-        self.fromaddr = fromaddr
-        self.toaddrs = toaddrs
-        self.subject = subject
-        self.setFormatter( logging.Formatter( "%(asctime)s %(dns)s %(filename)s %(levelname)s %(message)s", datefmt = '%Y-%m-%d %H:%M:%S', defaults = { "dns": socket.gethostname( ) } ) )
-
-    def flush(self):
-        if len(self.buffer) > 0:
-            import smtplib
-            port = self.mailport
-            if not port:
-                port = smtplib.SMTP_PORT
-            smtp = smtplib.SMTP( self.mailhost, port )
-            msg = f"From: {self.fromaddr}\r\nTo: {self.toaddrs}\r\nSubject: {self.subject}\r\n\r\n"
-            for record in self.buffer:
-                s = self.format( record )
-                print( s )
-                msg = msg + s + '\r\n'
-            msg = msg + '\r\n\r\n'
-            smtp.sendmail( self.fromaddr, self.toaddrs, msg )
-            smtp.quit( )
-            self.buffer = []
 
 
 
