2b72ff7389d61d7f502707ba8f7397db523a15ae
George Marselis <george.marselis@vetinst.no>
2025-10-08 19:31:22 +0200

    demux/discovery.py: Module to put methods for new run discovery

---
+++ demux/discovery.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.11
+
+"""
+
+Module to put methods for new run discovery
+
+"""
+
+
+########################################################################
+# displayNewRuns
+########################################################################
+
+def displayNewRuns( ):
+    """
+    buzz
+    """
+    return
