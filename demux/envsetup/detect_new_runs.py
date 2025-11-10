c556424d5e58a448951e5784b2b75baa6a59f8dc
George Marselis <george@marsel.is>
2025-11-10 21:13:23 +0100

    making variables for #122 more uniform with the res to the script, even though i will replace them when i get to it

--- demux/envsetup/detect_new_runs.py
+++ demux/envsetup/detect_new_runs.py
@@ -50,9 +50,9 @@ def detect_new_runs(  ):
     #
     # WHAT DOES THIS SCRIPT DO:
     #       This script sets up the demultiplexing:
-    #           Picks up all the existing folders within /data/rawdata/*M06578*_demultiplex in variable RunList     
+    #           Picks up all the existing folders within /data/rawdata/*M06578*_demultiplex in variable runList     
     #       Initializes a new variable
-    #           fills it up with the var from RunList
+    #           fills it up with the var from runList
     #           but removes the _demultiplex part
     #               demultiplexList.append(dirName.replace('_demultiplex',''))
     #       Differentiates the new item to run by comparing with the os.listdir(DemultiplexDir)
@@ -69,15 +69,15 @@ def detect_new_runs(  ):
     #               /data/bin/cron_out.log
 
 
-    RunList = []
+    runList = []
     print( f"==> Getting new rawdata directories started ==\n")
 
-    for dirName in os.listdir( demultiplex_script.demux.rawDataDir ): # add directory names from the raw generated data directory
+    for dirName in os.listdir( demux.rawDataDir ): # add directory names from the raw generated data directory
 
-        if demultiplex_script.demux.demultiplexDirSuffix in dirName: #  ignore any _demux dirs
+        if demux.demultiplexDirSuffix in dirName: #  ignore any _demux dirs
             continue
-        if any( tag in dirName for tags in [ demultiplex_script.demux.nextSeq, demultiplex_script.demux.miSeq ] for tag in tags ): # only add directories that have a sequncer tag
-            RunList.append( dirName )
+        if any( tag in dirName for tags in [ demux.nextSeq, demux.miSeq ] for tag in tags ): # only add directories that have a sequncer tag
+            runList.append( dirName )
 
     print( f"==< Getting new rawdata directories finished ==\n")
 
@@ -85,12 +85,12 @@ def detect_new_runs(  ):
     DemultiplexList = [] 
     print( f"==> Getting demultiplexed directories started ==\n")
 
-    for dirName in os.listdir( demultiplex_script.demux.demultiplexDir ):
+    for dirName in os.listdir( demux.demultiplexDir ):
 
-        if demultiplex_script.demux.demultiplexDirSuffix not in dirName: #  demultiplexed directories must have the  _demultiplex suffix # safety in case any other dirs included in /data/demultiplex
+        if demux.demultiplexDirSuffix not in dirName: #  demultiplexed directories must have the  _demultiplex suffix # safety in case any other dirs included in /data/demultiplex
             continue
-        if any( tag in dirName for tags in [ demultiplex_script.demux.nextSeq, demultiplex_script.demux.miSeq ] for tag in tags ): # ignore directories that have no sequncer tag
-            DemultiplexList.append( dirName.replace( demultiplex_script.demux.demultiplexDirSuffix, '' ) ) # null _demultiplex so we can compare the two lists below
+        if any( tag in dirName for tags in [ demux.nextSeq, demux.miSeq ] for tag in tags ): # ignore directories that have no sequncer tag
+            DemultiplexList.append( dirName.replace( demux.demultiplexDirSuffix, '' ) ) # null _demultiplex so we can compare the two lists below
 
     print( f"==> Getting demultiplexed directories finished ==\n")
 
@@ -98,64 +98,44 @@ def detect_new_runs(  ):
     # Right now the script operates on only one run at a time, but in the future we might want to run miltiple things at a time
 
     count = 0
-    NewRunList = [ ]
-    NewRunID = '' # turn this into an array
-    for item in RunList: # iterate over RunList to see if there a new item in DemultiplexList, effectively comparing the contents of the two directories
+    newRunList = [ ]
+    newRunID = '' # turn this into an array
+    for item in runList: # iterate over runList to see if there a new item in DemultiplexList, effectively comparing the contents of the two directories
         if item in DemultiplexList:
             count += 1
         else:
-            NewRunList.append( item )
-            NewRunID = item # any RunList item that is not in the demux list, gets processed
+            newRunList.append( item )
+            newRunID = item # any runList item that is not in the demux list, gets processed
 
     localTime = strftime( "%Y-%m-%d %H:%M:%S", localtime( ) ) 
-    print( f"{ localTime } - { len( RunList ) } in rawdata and { len( DemultiplexList ) } in demultiplex: ")
+    print( f"{ localTime } - { len( runList ) } in rawdata and { len( DemultiplexList ) } in demultiplex: ")
 
-    if count == len( RunList ): # no new items in DemultiplexList, therefore count == len( RunList )
+    if count == len( runList ): # no new items in DemultiplexList, therefore count == len( runList )
          print( 'all the runs have been demultiplexed\n' )
 
-    if NewRunID:
+    if newRunID:
 
-        flatNewRunList = ", ".join( NewRunList )
-        print( f"{len(NewRunList)} new items to demux: {flatNewRunList}")
+        flatNewRunList = ", ".join( newRunList )
+        print( f"{len(newRunList)} new items to demux: {flatNewRunList}")
 
-        print( f"Will work on this RunID: {NewRunID}\n" ) # caution: if the corresponding _demux directory is somehow corrupted (wrong data in SampleSheetFilename or incomplete files), this will be printed over and over in the log file
+        print( f"Will work on this RunID: {newRunID}\n" ) # caution: if the corresponding _demux directory is somehow corrupted (wrong data in SampleSheetFilename or incomplete files), this will be printed over and over in the log file
 
         # essential condition to process is that RTAComplete.txt and SampleSheet.csv
-        if demultiplex_script.demux.rtaCompleteFile in os.listdir( os.path.join( demultiplex_script.demux.rawDataDir, NewRunID ) ) and demultiplex_script.demux.sampleSheetFileName in os.listdir( os.path.join( demultiplex_script.demux.rawDataDir, NewRunID ) ):
+        if demux.rtaCompleteFile in os.listdir( os.path.join( demux.rawDataDir, newRunID ) ) and demux.sampleSheetFileName in os.listdir( os.path.join( demux.rawDataDir, newRunID ) ):
 
-            # if demultiplex_script.demux.debug: 
-                # print( f"{demultiplex_script.demux.python3_bin} {demultiplex_script.demux.scriptFilePath} {NewRunID}")
+            # if demux.debug: 
+                # print( f"{demux.python3_bin} {demux.scriptFilePath} {newRunID}")
 
-            if not os.path.exists( demultiplex_script.demux.scriptFilePath ):
-                print( f"{demultiplex_script.demux.scriptFilePath} does not exist!" )
+            if not os.path.exists( demux.scriptFilePath ):
+                print( f"{demux.scriptFilePath} does not exist!" )
                 exit( )
 
             # EXAMPLE: /bin/python3.11 /data/bin/current_demultiplex_script.py 210903_NB552450_0002_AH3VYYBGXK 
-            demultiplex_script.main( NewRunID )
-            # return NewRunID
+            main( newRunID )
+            # return newRunID
 
             print( 'completed\n' )
         else:
             print( ', waiting for the run to complete\n' )
-
-#
-########################################################################
-# Turning demultiplex_script into a python module, by placing it in a
-# package structure
-########################################################################
-#
-# from demultiplex.demultiplex_script import main
-#
-# def run_cron_job():
-#     RunID = "example_run_id"  # Replace with actual logic to retrieve RunID
-#     main(RunID)
-#
-# if __name__ == "__main__":
-#     run_cron_job()
-
-# to run this from cron, you change the command line to this
-# so python can find the demultiplexing script
-# PYTHONPATH=/data/bin /usr/bin/python3.11 /data/bin/cron_job.py
-
 
     demuxLogger.info( f"==< {demux.n}/{demux.totalTasks} tasks: Detecting if new runs exist finished\n")
