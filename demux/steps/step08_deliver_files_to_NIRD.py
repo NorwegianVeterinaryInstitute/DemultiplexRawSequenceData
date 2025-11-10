d24e747250a86e4881f60e5a5ebd77319ee027e7
George Marselis <george@marsel.is>
2025-11-10 22:19:51 +0100

    3 years after forseen it was a need, we are finally implementing #26

---
+++ demux/steps/step08_deliver_files_to_NIRD.py
@@ -0,0 +1,17 @@
+
+from demux.loggers import demuxLogger, demuxFailureLogger
+
+
+########################################################################
+# deliver_files_to_NIRD
+########################################################################
+
+def deliver_files_to_NIRD( demux ):
+    """
+    Make connection to NIRD and upload the data
+    """
+    demux.n = demux.n + 1
+    demuxLogger.info( f"==> {demux.n}/{demux.totalTasks} tasks: Preparing files for archiving to NIRD started\n")
+
+
+    demuxLogger.info( f"==< {demux.n}/{demux.totalTasks} tasks: Preparing files for archiving to NIRD finished\n")
