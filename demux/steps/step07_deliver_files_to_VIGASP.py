28e23a01dc6540a9fe1b65c391149746a5c381a7
George Marselis <george@marsel.is>
2025-11-10 22:15:09 +0100

    delivering automated files to vigasp: #27 , opened 3 years ago

---
+++ demux/steps/step07_deliver_files_to_VIGASP.py
@@ -0,0 +1,17 @@
+
+from demux.loggers import demuxLogger, demuxFailureLogger
+
+########################################################################
+# deliver_files_to_VIGASP
+########################################################################
+
+def deliver_files_to_VIGASP( demux ):
+    """
+    Write the uploader file needed to upload the data to VIGASP and then
+        upload the relevant files.
+    """
+    demux.n = demux.n + 1
+    demuxLogger.info( f"==> {demux.n}/{demux.totalTasks} tasks: Preparing files for uploading to VIGASP started\n")
+
+
+    demuxLogger.info( f"==< {demux.n}/{demux.totalTasks} tasks: Preparing files for uploading to VIGASP finished\n")
