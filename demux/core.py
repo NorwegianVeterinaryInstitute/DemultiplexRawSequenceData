35b29aaf18d886b4441e95d45ccf20983ee40749
George Marselis <george@marsel.is>
2025-11-11 18:38:16 +0100

    getting to a stable place to test runIDShort

--- demux/core.py
+++ demux/core.py
@@ -28,8 +28,6 @@ import demux.config.constants
 
 from sample_sheet import SampleSheet # https://sample-sheet.readthedocs.io/quick-start.html
 
-import demux.loggers as demux_logging
-
 
 """
 The demux object is the central configuration/state holder for the whole pipeline.  It:
@@ -116,7 +114,7 @@ class demux:
 
     ######################################################
     RunID                           = ""
-    runIDShort                      = ""  # demux.runIDShort vs demux.RunIDShort https://github.com/NorwegianVeterinaryInstitute/DemultiplexRawSequenceData/issues/126 
+    runIDShort                      = ""  # https://github.com/NorwegianVeterinaryInstitute/DemultiplexRawSequenceData/issues/126
     rawDataRunIDdir                 = ""
     demultiplexRunIDdir             = ""
     demultiplexLogDirPath           = ""
