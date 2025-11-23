046c2c044e5ad2085c8c601f4bd9e4cc709d9dcc
George Marselis <george.marselis@vetinst.no>
2025-11-23 18:10:39 +0100

    adding more constants

--- demux/config/constants.py
+++ demux/config/constants.py
@@ -2,11 +2,25 @@
 This is a file with all the constant-constants: things that should be parametrized but they will never change
 '''
 
+# directory names
+DATA_ROOT_DIR           = "/data"
+RAW_DATA_DIR_NAME       = "rawdata"
+DEMULTIPLEX_DIR_NAME    = "demultiplex"
+FOR_TRANSFER_DIR_NAME   = "for_transfer"
+SAMPLESHEET_DIR_NAME    = "samplesheets"
+LOG_DIR_NAME            = "log"
+MULTIQC_DATA_DIR_NAME	= "multiqc_data"
+
+# suffixes
+CSV_SUFFIX 				= ".csv"
+QC_SUFFIX               = "_qc"
+DEMULTIPLEX_DIR_SUFFIX 	= "_demultiplex"
+ZIP_SUFFIX              = '.zip'
+COMPRESSED_FASTQ_SUFFIX = '.fastq.gz' 
+TAR_SUFFIX              = '.tar'
+
 MD5_SUFFIX              = ".md5"
 MD5_LENGTH              = 16  # 128 bits
 SHA512_SUFFIX           = ".sha512"
 SHA512_LENGTH           = 64  # 512 bits
-ZIP_SUFFIX              = '.zip'
-COMPRESSED_FASTQ_SUFFIX = '.fastq.gz' 
-TAR_SUFFIX              = '.tar'
 
