d0c4e1631fb42bce2e3babbe712edcacb388d3ea
George Marselis <george.marselis@vetinst.no>
2026-01-07 18:45:54 +0200

    once more

--- demux/steps/step08_deliver_files_to_NIRD.py
+++ demux/steps/step08_deliver_files_to_NIRD.py
@@ -138,9 +138,10 @@ def _upload_and_verify_file_via_ssh_2fa( demux, tar_file ):     # worker per fil
 
     pprint.pprint( f"username: {username} | password: {password} | TOTP: {totp}" )
 
-    sys.exit( f"{sys._getframe( ).f_code.co_name} is not yet implemented" )
 
     demuxLogger.info( termcolor.colored( f"==< {demux.n}/{demux.totalTasks} tasks: Uploading file {tar_file} via ssh 2FA finished\n", color="red", attrs=["bold"] ) )
+
+    sys.exit( f"{sys._getframe( ).f_code.co_name} is not yet implemented" )
 
 
 def _upload_and_verify_file_via_ssh( demux, tar_file ):  # worker per file, tar_file is in absolute path format
