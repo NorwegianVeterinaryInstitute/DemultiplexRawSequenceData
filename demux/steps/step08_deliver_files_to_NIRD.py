c3548b97a07a6199b9c2a4d8340a3514e87c164a
George Marselis <george.marselis@vetinst.no>
2026-01-08 00:09:52 +0200

    same

--- demux/steps/step08_deliver_files_to_NIRD.py
+++ demux/steps/step08_deliver_files_to_NIRD.py
@@ -13,7 +13,7 @@ import urllib.request
 
 from typing import Tuple
 
-from paramiko import SSHClient, SSHConfig, AutoAddPolicy, RejectPolicy, Transport
+from paramiko import SSHClient, SSHConfig, AutoAddPolicy, RejectPolicy
 from paramiko.ssh_exception import AuthenticationException
 from scp import SCPClient
 
