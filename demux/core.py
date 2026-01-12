cf355c8aa82d26b2577a2af7ac2afb278f4d29ae
George Marselis <george.marselis@vetinst.no>
2026-01-12 02:12:04 +0200

    some live testing

--- demux/core.py
+++ demux/core.py
@@ -179,9 +179,8 @@ class demux:
     # nird_upload_host                = "laptop"
     nird_scp_port                   = "22" # https://documentation.sigma2.no/getting_help/two_factor_authentication.html#how-to-copy-files-without-using-2fa-otp
     nird_username                   = "gmarselis" # change this to be the user running the script
-    # nird_base_upload_path_ssh       = "/nird/projects/NS9305K/SEQ-TECH/data_delivery"
-    nird_base_upload_path_ssh       = "/nird/datapeak/NS9305K/georgetest"
-    # nird_base_upload_path_ssh       = "/data/tmp/nird"
+    # nird_base_upload_path_ssh       = "/nird/projects/NS9305K/SEQ-TECH/data_delivery" # directory location before datapeak
+    nird_base_upload_path_ssh       = "/nird/datapeak/NS9305Kgmarselis/demux_transfer_test"
     nird_base_upload_path_local     = "/data/tmp/nird"
     nird_base_upload_path           = ""
     nird_key_filename               = "/home/gmarselis/.ssh/id_ed25519.3jane"
