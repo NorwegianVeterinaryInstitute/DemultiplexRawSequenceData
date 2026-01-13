f671d9eda297f00afc0dd6b7df139e3d74100847
George Marselis <george.marselis@vetinst.no>
2026-01-13 20:01:19 +0200

    testing ssh with keys

--- demux/core.py
+++ demux/core.py
@@ -175,7 +175,8 @@ class demux:
     allowed_nird_access_modes       = [ "ssh", "ssf_2fa" "mounted" ]
     nird_copy_mode                  = "parallel"
     allowed_nird_copy_modes         = [ "serial", "parallel" ]
-    nird_upload_host                = "login.nird.sigma2.no"
+    # nird_upload_host                = "login.nird.sigma2.no"
+    nird_upload_host                = "rei.vetinst.no"
     # nird_upload_host                = "laptop"
     nird_scp_port                   = "22" # https://documentation.sigma2.no/getting_help/two_factor_authentication.html#how-to-copy-files-without-using-2fa-otp
     nird_username                   = "gmarselis" # change this to be the user running the script
