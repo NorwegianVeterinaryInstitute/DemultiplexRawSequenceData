4a7cc796411f77dab5d9a737b5252a30092f1075
George Marselis <george.marselis@vetinst.no>
2026-03-08 13:14:18 +0200

    more fixing

--- demux/steps/step07_deliver_files_to_VIGASP.py
+++ demux/steps/step07_deliver_files_to_VIGASP.py
@@ -6,16 +6,16 @@ from demux.loggers import demuxLogger, demuxFailureLogger
 # deliver_files_to_VIGASP
 ########################################################################
 
-# def deliver_files_to_VIGASP( demux ):
-#     """
-#     Write the uploader file needed to upload the data to VIGASP and then
-#         upload the relevant files.
-#     """
-#     demux.n = demux.n + 1
-#     demuxLogger.info( f"==> {demux.n}/{demux.totalTasks} tasks: Preparing files for uploading to VIGASP started\n")
+def deliver_files_to_VIGASP( demux ):
+    """
+    Write the uploader file needed to upload the data to VIGASP and then
+        upload the relevant files.
+    """
+    demux.n = demux.n + 1
+    demuxLogger.info( f"==> {demux.n}/{demux.totalTasks} tasks: Preparing files for uploading to VIGASP started\n")
 
 
-#     demuxLogger.info( f"==< {demux.n}/{demux.totalTasks} tasks: Preparing files for uploading to VIGASP finished\n")
+    demuxLogger.info( f"==< {demux.n}/{demux.totalTasks} tasks: Preparing files for uploading to VIGASP finished\n")
 
 import dataclasses
 import json
