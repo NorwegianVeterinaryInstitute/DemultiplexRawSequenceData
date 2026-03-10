48e4d4d1d82cff56601eae34db226c20db153afd
George Marselis <george.marselis@vetinst.no>
2026-03-11 01:03:07 +0200

    typo

--- demux/core.py
+++ demux/core.py
@@ -283,7 +283,7 @@ class demux:
 
         for sample in sample_sheet.samples:
             if sample is None or sample.Transfer_VIGAS is None or sample.Transfer_NIRD is None or sample.NIRD_Location is None:
-                message = "ValueError: Current Sample sheet does not contain needed fields required for file transfer. Abording."
+                message = "ValueError: Current Sample sheet does not contain needed fields required for file transfer. Aborting."
                 raise ValueError( message )
             project_samples_metadata[ sample.Sample_Project ][ sample.Sample_ID ] = {
                 'transfer_to_vigas': sample.Transfer_VIGAS.lower( ) == "yes",
